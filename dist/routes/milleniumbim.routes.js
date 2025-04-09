78c0968c35c8a4b5567e854f533ab99a88874a3b
William-Cossa <devwilliamcossa@gmail.com>
2025-04-09 12:55:20 +0200

    remove dist directory

--- dist/routes/milleniumbim.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createCheckoutSession, startCheckout, sucessPaymentCheck, } from "../controllers/milleniumbim.controller.js";
-const millleniumBimRouter = Router();
-millleniumBimRouter.post("/millenium-bim", createCheckoutSession);
-millleniumBimRouter.get("/millenium-bim/checkout", startCheckout);
-millleniumBimRouter.get("/millenium-bim/sucess", sucessPaymentCheck);
-export default millleniumBimRouter;
--- dist/utils/generateOrderId.js
+++
@@ -1,5 +0,0 @@
-export function generateOrderId() {
-    const prefix = Math.random().toString(36).substring(2, 7).toUpperCase();
-    const timestamp = Date.now();
-    return `${prefix}${timestamp}`;
-}
